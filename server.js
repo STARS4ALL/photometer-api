b72595058f6fb3e23122554cc962a61a6bca6a72
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2016-07-27 14:59:33 +0200

    Import configuration variables

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const bodyParser = require('body-parser');
 var Tess = require('./models/tess.js').Tess;
 var Observations = require('./models/observation.js').Observations;
 var Report = require('./models/report.js').Report;
+var config = require('./config.js');
 
 const PORT = 8888;
 
 //DB Models
 
 
-mongoose.connect('mongodb://photometerdb/photometers', function(err,res) {
+mongoose.connect(DATABASE_HOST+DATABASE_NAME, function(err,res) {
 
 	if(err){
 		console.log('ERROR: connecting with db ' + err + ' '+res);
@@ -29,7 +30,11 @@ var app = express();
 
 //app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var router = express.Router();
 
@@ -37,18 +42,18 @@ router.get('/', function (req, res){
 	res.json({ message: 'Hello world\n' });
 });
 
-router.get('/api/info', function (req, res){
+router.get('/api/v1/info', function (req, res){
 	res.json({project:'STARS4ALL', description: 'This is a REST API to access to the photometers data', funded:' By European Union ( H2020 framework -> 688135)'});
 });
 
-router.get('/api/photometers', function (req, res){
+router.get('/api/v1/photometers', function (req, res){
 
 	Tess.find({}, function (errs,docs){
 		res.json(docs);
 	});
 });
 
-router.get('/api/photometers/:id', function (req, res){
+router.get('/api/v1/photometers/:id', function (req, res){
 
 	Tess.findOne({name:req.params.id}, function (errs, docs){
 	
@@ -61,20 +66,106 @@ router.get('/api/photometers/:id', function (req, res){
 
 });
 
-router.get('/api/observations/:id', function(req, res){
+router.get('/api/v1/photometers/:id_tess/observations/:id_obs', function(req, res){
 
-	var page = (req.query.page) ? req.query.page : 0; 
-	var startItem = page*20;
+	
 
+	//Read fields
+	var fields = req.query.fields;
+	if (fields!=null){
+		fields = fields.replace(/,/g," ");
+	} else {
+		fields = '';
+	}
 
-	Observations.find({name:req.params.id}, 'name seq freq mag tamb tsky wdBm', {skip: startItem, limit:20}, function (errs, docs){
-		res.json(docs);
+	Observations.findOne({ _id: req.params.id_obs}, fields,  function (errs, doc){
+		res.json(doc);
 	});
 	
 
 });
 
-router.post('/api/reports/new', function(req, res){
+router.get('/api/v1/photometers/:id_tess/observations', function(req, res){
+
+	var id_tess = req.params.id_tess;
+
+	var link = req.protocol+"://"+req.hostname+":"+PORT+"/api/v1/photometers/"+id_tess+"/observations/"; 
+
+	var query;
+
+	//Read fields
+	var fields = req.query.fields;
+	if (fields!=null){
+		fields = fields.replace(/,/g," ");
+		fields = fields + ' _id';
+	} else {
+		fields = '_id';
+	}
+	//Pagination variables
+	var maxCount = 100;
+	var cursor = (req.query.cursor) ? req.query.cursor : 0; 
+	var count = (req.query.count) ? req.query.count : maxCount;
+	var aux = (cursor-count);
+	var before = ( aux < 0) ? 0 : aux;
+	var after =parseInt(cursor)+parseInt(count);
+	var link = req.protocol+"://"+req.hostname+":"+PORT+"/api/v1/photometers/"+id_tess+"/observations/";
+
+
+/*	var time = req.query.tstamp;
+	var q2;
+	if (time!=null){
+		query["tstamp"]=/time/;
+		q2 = {name:id_tess, tstamp:/time/};
+	}else{
+		q2 = {name:id_tess};
+	}
+	console.log(query+" "+time);
+*/	
+	var beginInterval = req.query.begin;
+	var endInterval = req.query.end;
+
+	if ((beginInterval != null) && (endInterval!=null)){
+		query = {name:id_tess, tstamp: {$gt : beginInterval, $lt: endInterval}};		
+	} else if ((beginInterval != null) && (endInterval == null)){
+		query = {name:id_tess, tstamp : {$gt: beginInterval}};
+	} else {
+		query = {name:id_tess};
+	}
+	console.log("Query: %j", query);
+	Observations.find(query, fields ,{skip: cursor, limit:count} , function (errs, docs){
+	
+		if (errs){
+			res.json(500);
+		} else {
+
+		
+
+			Observations.count({name:id_tess}, function(err, c){
+				
+				res.append('X-Total-Count',c);
+
+				var link_previous = link+"?cursor="+before+"&count="+count;
+				var link_next = link+"?cursor="+after+"&count="+count;
+				var data_result = JSON.parse(JSON.stringify(docs));
+		
+				for (var i in docs){
+					data_result[i]["links"]={self: link+data_result[i]["_id"]};
+					delete data_result[i]["_id"];
+				}
+				res.links({
+					next: link_next,
+					prev: link_previous
+				});
+
+				res.json(data_result);
+			});
+	
+		}
+	});
+
+});
+
+router.post('/api/v1/reports/new', function(req, res){
 	var photometers = req.body.photometers;
 	var sensors = req.body.sensors
 
@@ -108,7 +199,17 @@ router.post('/api/reports/new', function(req, res){
 
 });
 
-router.get('/api/reports/:id', function(req, res){
+router.get('/api/v1/reports', function(req, res){
+	Report.find({}, function (errs,docs){
+		if (errs){
+			res.json(500);
+		}else {
+			res.status(200).json(docs);
+		}
+	});
+});
+
+router.get('/api/v1/reports/:id', function(req, res){
 
 	var maxCount = 100;
 	//var limit = req.query.limit;
@@ -118,7 +219,7 @@ router.get('/api/reports/:id', function(req, res){
 	var aux = (cursor-count);
 	var before = ( aux < 0) ? 0 : aux;
 	var after =parseInt(cursor)+parseInt(count);
-	var link = req.protocol+"://"+req.hostname+"/api/reports/"+req.params.id;
+	var link = req.protocol+"://"+req.hostname+":"+PORT+"/api/reports/"+req.params.id;
 
 	//Looking for a report
 	Report.findOne({_id:req.params.id}, function (errs, docs){
@@ -138,8 +239,13 @@ router.get('/api/reports/:id', function(req, res){
 				} else {
 					var link_previous = link+"?cursor="+before+"&count="+count;
 					var link_next = link+"?cursor="+after+"&count="+count;
-					var result = "{data:"+JSON.stringify(obs_docs,null,4)+", paging:{cursors:{after:"+after+",before:"+before+"},previous:"+link_previous+",next:"+link_next+"}}";
-					res.json(result);
+					//var result = "{ data: "+JSON.stringify(obs_docs,null,' ')+"}"
+					//var result2 = { data: obs_docs, paging: { cursors : { after : after, before : before}, previous: link_previous, next:link_next}}
+					res.links({
+						prev: link_previous,
+						next: link_next
+					});
+					res.json(obs_docs);
 				}
 			});
 		}
